16b22f32580bbbcb6fd8d8c6cda7af4aa5c460ab
iabhimanyusharma <81915715+iabhimanyusharma@users.noreply.github.com>
2022-04-09 16:37:33 +0530

    update

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const _ = require('lodash');
+const readFile = require('fs');
 
-const items = [1,[2,[3,[4,]]]];
-
-const newItems = _.flattenDeep(items); // we are using lodash as a dependency here to flatten this array
-// this functionality would not work if we hadnt installed the package.
-
-console.log(newItems);
-
+readFile('./content/first.txt', 'utf8', (err, ata) => {
+    if (err) {
+        console.log(err);
+        return;
+    } else {
+        console.log(data);
+        return;
+    }
+})
